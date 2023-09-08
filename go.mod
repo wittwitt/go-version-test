d6deba6cb68812efbff702c3886ed1ef6de2c661
Witt <i5d@live.com>
2023-09-08 17:34:48 +0800

    remov xerror

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.20
 require (
 	github.com/BurntSushi/toml v1.3.2
 	github.com/urfave/cli/v2 v2.25.7
-	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
 )
 
 require (
--- lib1/lib1.go
+++ lib1/lib1.go
@@ -2,15 +2,11 @@ package lib1
 
 import (
 	"fmt"
-
-	"golang.org/x/xerrors"
 )
 
 func Say() {
 	fmt.Println("gov lib1 say: hi: commit 0.0.8")
 
-	err := xerrors.New("x")
-	fmt.Println("add xerr", err)
 }
 
 func Say2() {
