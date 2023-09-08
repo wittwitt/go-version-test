3e1f073cc10b5a06a29ddf93a03d796aeda83ad6
Witt <i5d@live.com>
2023-09-08 16:06:10 +0800

    0.0.4 lib add xerror

--- lib1/lib1.go
+++ lib1/lib1.go
@@ -2,9 +2,13 @@ package lib1
 
 import (
 	"fmt"
+
+	"golang.org/x/xerrors"
 )
 
 func Say() {
 	fmt.Println("gov lib1 say: hi: commit 0.0.4")
 
+	err := xerrors.New("x")
+	fmt.Println("add xerr", err)
 }
